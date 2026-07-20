abc9bdb5526aba4e69aecd6c76519441dcb90770
dbbreinar-sudo <dbbreinar@gmail.com>
2026-07-20 11:58:56 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const FIREBASE_URL = "https://TU-PROYECTO.firebaseio.com/geocercas.json";
+
+app.get("/geocercas", async (req, res) => {
+  try {
+    const response = await axios.get(FIREBASE_URL);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener datos" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Servidor puente funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
