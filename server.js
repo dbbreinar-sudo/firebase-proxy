a4be81ac95e4292b3c8290998fdc609d2d2a4f94
dbbreinar-sudo <dbbreinar@gmail.com>
2026-07-20 12:26:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const FIREBASE_URL = "https://gps-moto-tesis-default-rtdb.firebaseio.com/geocercas";
+const FIREBASE_URL = "https://gps-moto-tesis-default-rtdb.firebaseio.com/geocercas.json";
 
 app.get("/geocercas", async (req, res) => {
   try {
